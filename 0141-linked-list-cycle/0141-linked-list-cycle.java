b427c86d9c94cbe0ccca05972401f02856d82bad
Ali Mehdi Naqvi <143258018+alimehdi04@users.noreply.github.com>
2024-06-07 01:24:35 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (81.74%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,25 +9,42 @@
  *     }
  * }
  */
-public class Solution {
-    public boolean hasCycle(ListNode head) {
-          if (head == null || head.next == null) {
-            return false;
-        }
-
+public class Solution 
+{
+    public boolean hasCycle(ListNode head) 
+    {
         ListNode slow = head;
         ListNode fast = head;
-
-        while (fast.next != null && fast.next.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-
-            if (slow == fast) {
+        
+        while(fast!=null&&fast.next!=null)
+        {
+                slow = slow.next;
+                fast = fast.next.next;
+            if(fast==slow)
                 return true;
-            }
-        }
 
+        }
         return false;
+        
+        // if(head!=null&&head.next!=null)
+        // {
+        //     slow = slow.next;
+        //     fast = fast.next.next;
+        // while(slow!=fast&&slow!=null&&fast!=null&&slow.next!=null&&fast.next.next!=null)
+        // {
+        //     slow = slow.next;
+        //     fast = fast.next.next;
+        // }
+        // fast = head;
+        // while(slow!=null&&fast!=null)
+        // {
+        //     slow = slow.next;
+        //     fast = fast.next;
+        //     if(slow==fast)
+        //         return true;
+        // }
+        // }
+        // return false;
         
     }
 }
