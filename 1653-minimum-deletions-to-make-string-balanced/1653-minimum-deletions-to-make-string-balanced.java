eddf7719c1670c84d6ef86d6dd63770c576735b7
Ali Mehdi Naqvi <143258018+alimehdi04@users.noreply.github.com>
2024-07-31 00:06:52 +0530

    Time: 44 ms (25.07%), Space: 45.4 MB (39.55%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -2,7 +2,7 @@ class Solution {
     public int minimumDeletions(String s) {
         int n = s.length();
         //int lbc[] = new int[n];
-        int rac[] = new int[n];
+        //int rac[] = new int[n];
         int min = Integer.MAX_VALUE;
         int tb = 0;
         int ta = 0;
@@ -14,15 +14,18 @@ class Solution {
         // }
         for(int i = n-1; i>=0; i--)
         {
-            rac[i] = ta;
             if(s.charAt(i)=='a')
                 ta++;
         }
         for(int i = 0; i < n; i++)
         {
             //lbc[i] = tb;
-            if(min>(rac[i]+tb))
-                min = (rac[i]+tb);
+            if(s.charAt(i)=='a')
+                ta--;
+             
+            if(min>(ta+tb))
+                min = (ta+tb);
+            
             if(s.charAt(i)=='b')
                 tb++;
             
