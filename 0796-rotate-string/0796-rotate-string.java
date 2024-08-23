8d62a6e657943bd88c8ca956cff7b216f721f0b3
Ali Mehdi Naqvi <143258018+alimehdi04@users.noreply.github.com>
2024-08-23 17:09:25 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (89.60%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -4,14 +4,6 @@ class Solution
     {
         if(s.length()!=goal.length())
             return false;
-        String combined = s.concat(s);
-        for(int i = 0; i < s.length(); i++)
-        {
-            String temp = combined.substring(i, i+s.length());
-            System.out.println(temp);
-            if(temp.equals(goal))
-                return true;
-        }
-        return false;
+        return (s + s).contains(goal);
     }
 }
