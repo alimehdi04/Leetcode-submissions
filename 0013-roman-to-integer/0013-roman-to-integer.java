2d973f08196f9b9750bc9de0bd9db51ed683b54b
Ali Mehdi Naqvi <143258018+alimehdi04@users.noreply.github.com>
2024-08-27 23:53:25 +0530

    Time: 5 ms (57.98%), Space: 44.7 MB (53.01%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -12,19 +12,20 @@ class Solution
         map.put('M',1000);
         
         int ans = 0;
+        int n = s.length();
         for(int i = 0; i < s.length(); i++)
         {
-            if((i+1)<s.length() && s.charAt(i) == 'C' && (s.charAt(i+1) == 'D'||s.charAt(i+1) == 'M'))
-{                ans = ans + map.get(s.charAt(i+1)) - map.get(s.charAt(i));
-                i++;}
-            else if((i+1)<s.length() && s.charAt(i) == 'X' && (s.charAt(i+1) == 'L'||s.charAt(i+1) == 'C'))
-{                ans = ans + map.get(s.charAt(i+1)) - map.get(s.charAt(i));
-                i++;}
-            else if((i+1)<s.length() && s.charAt(i) == 'I' && (s.charAt(i+1) == 'V'||s.charAt(i+1) == 'X'))
-{                ans = ans + map.get(s.charAt(i+1)) - map.get(s.charAt(i));
-                i++;}
-            else
-                ans += map.get(s.charAt(i));
+            int currentVal = map.get(s.charAt(i));
+            
+            if (i + 1 < n && currentVal < map.get(s.charAt(i + 1))) 
+            {
+                ans += map.get(s.charAt(i + 1)) - currentVal;
+                i++; // Skip the next character
+            } 
+            else 
+            {
+                ans += currentVal;
+            }
         }
         return ans;
     }
