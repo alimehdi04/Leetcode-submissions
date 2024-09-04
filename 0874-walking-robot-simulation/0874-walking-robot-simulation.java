0a938414d694dee88a29bb805fb04fefa01fbf95
Ali Mehdi Naqvi <143258018+alimehdi04@users.noreply.github.com>
2024-09-04 22:59:44 +0530

    Time: 27 ms (59.83%), Space: 54.9 MB (15.28%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.java
@@ -0,0 +1,88 @@
+class Solution 
+{
+    public int robotSim(int[] commands, int[][] obstacles) 
+    {
+        int n = commands.length;
+        int x=0,y=0,i=0,d=0,max=0;
+        HashSet<List<Integer>> set = new HashSet<>();
+        for(int k=0;k<obstacles.length;k++){
+            List<Integer> list = new ArrayList<>();
+            list.add(obstacles[k][0]);
+            list.add(obstacles[k][1]);
+            set.add(list);
+        }
+        for(;i<n;i++){
+            if(commands[i]==-2){
+                d--;
+                if(d==-1){
+                    d=3;
+                }
+            }
+            else if (commands[i]==-1){
+                d++;
+                if(d==4){
+                    d=0;
+                }
+            }
+            else{
+                int sum = commands[i];
+                if(d==0){
+                    while(sum!=0){
+                        y++;
+                        List<Integer> list = new ArrayList<>();
+                        list.add(x);
+                        list.add(y);
+                        if(set.contains(list)){
+                            y--;
+                            break;
+                        }
+                        sum--;
+                    }
+                }
+                else if(d==1){
+                    while(sum!=0){
+                        x++;
+                        List<Integer> list = new ArrayList<>();
+                        list.add(x);
+                        list.add(y);
+                        if(set.contains(list)){
+                            x--;
+                            break;
+                        }
+                        sum--;
+                    }
+                }
+                else if(d==2){
+                    while(sum!=0){
+                        y--;
+                        List<Integer> list = new ArrayList<>();
+                        list.add(x);
+                        list.add(y);
+                        if(set.contains(list)){
+                            y++;
+                            break;
+                        }
+                        sum--;
+                    }
+                }
+                else{
+                    while(sum!=0){
+                        x--;
+                        List<Integer> list = new ArrayList<>();
+                        list.add(x);
+                        list.add(y);
+                        if(set.contains(list)){
+                            x++;
+                            break;
+                        }
+                        sum--;
+                    }
+                }
+            }
+            if(max<((x*x)+(y*y))){
+                max = (x*x)+(y*y);
+            }
+        }
+        return max;
+    }
+}
