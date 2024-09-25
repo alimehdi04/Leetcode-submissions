a3dd81a10bbf058467869013c3287429c5e0764a
Ali Mehdi Naqvi <143258018+alimehdi04@users.noreply.github.com>
2024-09-25 18:23:50 +0530

    Time: 2 ms (33.20%), Space: 42.2 MB (69.02%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,26 +1,39 @@
-class Solution {
-    public int myAtoi(String s) {
+class Solution 
+{
+    public int myAtoi(String s) 
+    {
         int polarity = 1;
         int n = s.length();
         long result = 0; // Use long to handle overflow during calculation
         int sp = 0; //supervision
         
-        for (char c : s.toCharArray()) {
-            if (Character.isWhitespace(c) && sp == 0) {
+        for (char c : s.toCharArray()) 
+        {
+            if (Character.isWhitespace(c) && sp == 0) 
+            {
                 continue;
-            } else if (c == '-' && sp == 0) {
+            } 
+            else if (c == '-' && sp == 0) 
+            {
                 polarity = -1;
                 sp++;
-            } else if (c == '+' && sp == 0) {
+            } 
+            else if (c == '+' && sp == 0) 
+            {
                 sp++;
-            } else if (Character.isDigit(c)) {
+            } 
+            else if (Character.isDigit(c)) 
+            {
                 result = result * 10 + (c - '0');
                 sp++;
                 // Check for overflow
-                if (result > Integer.MAX_VALUE) {
+                if (result > Integer.MAX_VALUE) 
+                {
                     return polarity == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
                 }
-            } else {
+            } 
+            else 
+            {
                 break;
             }
         }
