5ccfb45815b04c01bb7bbab9359b44fddb913b8e
Ali Mehdi Naqvi <143258018+alimehdi04@users.noreply.github.com>
2024-10-03 00:32:17 +0530

    Time: 29 ms (50.91%), Space: 62.5 MB (29.84%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -2,52 +2,25 @@ class Solution
 {
     public int[] arrayRankTransform(int[] arr) 
     {
-        if (arr == null || arr.length == 0) return new int[0];
+        int[] sortedArr = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(sortedArr);
         
-        int n = arr.length;
-        int[][] indexedArr = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            indexedArr[i] = new int[]{arr[i], i};
+        HashMap<Integer, Integer> rank = new HashMap<>();
+        int rankcnt = 1;
+        
+        for(int num : sortedArr)
+        {
+            if(!rank.containsKey(num))
+                rank.put(num, rankcnt++);
         }
-
-        // Sort based on values
-        Arrays.sort(indexedArr, (a, b) -> Integer.compare(a[0], b[0]));
-
-        int[] result = new int[n];
-        int rank = 1;
-
-        result[indexedArr[0][1]] = rank;
-
-        for (int i = 1; i < n; i++) {
-            // If current element is different from previous, increase rank
-            if (indexedArr[i][0] != indexedArr[i-1][0]) {
-                rank++;
-            }
-            result[indexedArr[i][1]] = rank;
+        
+        int[] ans = new int[arr.length];
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+            ans[i] = rank.get(arr[i]);
         }
-
-        return result;
         
-        
-//         int[] sortedArr = Arrays.copyOf(arr, arr.length);
-//         Arrays.sort(sortedArr);
-        
-//         HashMap<Integer, Integer> rank = new HashMap<>();
-//         int rankcnt = 1;
-        
-//         for(int num : sortedArr)
-//         {
-//             if(!rank.containsKey(num))
-//                 rank.put(num, rankcnt++);
-//         }
-        
-//         int[] ans = new int[arr.length];
-        
-//         for(int i = 0; i < arr.length; i++)
-//         {
-//             ans[i] = rank.get(arr[i]);
-//         }
-        
-//         return ans;
+        return ans;
     }
 }
