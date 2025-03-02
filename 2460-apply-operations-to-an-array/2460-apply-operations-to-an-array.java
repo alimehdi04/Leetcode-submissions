1240e6ba5e813ec95abc1d93db07c4746eee1402
Ali Mehdi Naqvi <143258018+alimehdi04@users.noreply.github.com>
2025-03-02 23:49:29 +0530

    Time: 0 ms (100%), Space: 44.5 MB (8.8%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int length = nums.length;
+        int [] arr = new int[length];
+
+        for (int i = 0 , j = 0; i < length; i++)
+        {
+            if(nums[i] == 0) continue;
+            
+            if (i < length-1 && nums[i] == nums[i+1])
+            {
+                arr[j++] = 2 * nums[i];
+                nums[++i] = 0;  // or i++;  only
+            }else{
+                arr[j++] = nums[i];
+            }
+        }
+        return arr;
+    }
+}
