4e166015318bd007eb37dd2a552d112422e88b72
Ali Mehdi Naqvi <143258018+alimehdi04@users.noreply.github.com>
2026-02-02 18:28:56 +0530

    Time: 5 ms (23.81%), Space: 46 MB (13.89%) - LeetHub

---
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minimumCost(int[] v) {
+        int n = v.length;
+        int s = v[0];
+        Arrays.sort(v, 1, n);
+        s = s + v[1] + v[2];
+        return s;
+    }
+}
